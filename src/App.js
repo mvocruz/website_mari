3be566bb34e4924ca903418a0ea77b88c024be1b
Marcio Cruz <marciocruz@Marcios-MacBook-Pro.local>
2021-02-01 19:37:45 -0800

    create login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import './App.css';
 import {Route, BrowserRouter as Router, Link} from "react-router-dom";
 import Home from './Pages/Home';
@@ -7,6 +8,7 @@ import Contact from './Pages/Contact';
 import Services from './Pages/Services';
 import Portfolio from './Pages/Portfolio';
 import Faq from './Pages/Faq';
+import Login from './Pages/Login';
 
 function App() {
   return ( 
@@ -24,6 +26,8 @@ function App() {
             <Link to="/services" className="nav-services">Services</Link>
             <Link to="/contact" className="nav-contact">Contact</Link>
             <Link to="/faq" className="nav-faq">F.A.Q.</Link>
+            <Button component={Link} to="/login" className="nav-login" variant="outlined">Login</Button>
+            
           </div>
         </nav>
       </header>  
@@ -34,9 +38,28 @@ function App() {
           <Route path="/services" exact component={Services}/>
           <Route path="/portfolio" exact component={Portfolio}/>
           <Route path="/faq" exact component={Faq}/>
+          <Route path="/login" exact component={Login}/>
 
       <footer className="App-footer">
-        <text>@ 2021 Brazilian Blowout</text>
+      <div className='app-footer-social'>
+        <div className='app-footer-social-box'>
+          <div className='app-footer-icon-instagram'>
+            <i class="fab fa-instagram fa-2x"></i>
+          </div>
+          <div className='app-footer-icon-facebook'>
+            <i class="fab fa-facebook fa-2x"></i>
+          </div>
+        </div>
+        <div className='app-footer-link-box'>
+          <Link to="/" className="footer-home">Home</Link>
+          <Link to="/about" className="footer-about">About</Link>
+          <Link to="/portfolio" className="footer-portfolio">Portfolio</Link>
+          <Link to="/services" className="footer-services">Services</Link>
+          <Link to="/contact" className="footer-contact">Contact</Link>
+          <Link to="/faq" className="footer-faq">F.A.Q.</Link>
+        </div>
+      </div>
+        <text style={{paddingTop:50}}>@ 2021 Brazilian Blowout</text>
       </footer>
     </div>
   </Router>
---
+++ src/Pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+// import {Route, BrowserRouter as Router} from "react-router-dom";
+
+function Login() {
+  return (
+    <div className='page-login'>
+      <text style={{fontSize: 70}}>Login Page</text>    
+    </div>
+  );
+}
+
+export default Login;
