21b8149b458cb0495b9a36df31fe5de1a7e3e962
Marcio Cruz <marciocruz@Marcios-MacBook-Pro.local>
2021-02-02 20:40:41 -0800

    change background and include input field for login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import './Login.css';
-// import {Route, BrowserRouter as Router} from "react-router-dom";
+import { Button, TextField } from "@material-ui/core";
 
-function Login() {
+import './Login.css';
+
+
+export default function Login() {
+
   return (
     <div className='page-login'>
-      <text style={{fontSize: 70}}>Login Page</text>    
+      <text className='page-login-title' style={{fontSize:70, padding:30}}>LOGIN</text>
+      <div className='page-login-logo'>
+       <i class="fas fa-user-lock fa-3x"></i>
+      </div>
+      <div className='page-login-input'>
+        <div className='page-login-input-text'>
+          <TextField
+            label="Email"
+            style={{ margin: 8 }}
+            // value={values.project_name}
+            name="Email"
+            InputLabelProps={{
+                shrink: true
+            }}
+            fullWidth
+            placeholder="Email"
+            margin="normal"
+            variant="outlined"
+          /> 
+          <TextField
+            label="Password"
+            style={{ margin: 8 }}
+            // value={values.project_name}
+            name="Password"
+            InputLabelProps={{
+                shrink: true
+            }}
+            fullWidth
+            placeholder="Password"
+            margin="normal"
+            variant="outlined"
+          /> 
+        </div>
+        <div className='page-login-input-button'>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            // component={Link}
+            // to={{
+            //     pathname: "/projectdashboard",
+            //     aboutProps: {
+            //         id: props.Pid,
+            //         isManager: 1
+            //     }
+            // }}
+            // onClick={onUpdate}
+        >
+            Login
+            </Button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Login;
